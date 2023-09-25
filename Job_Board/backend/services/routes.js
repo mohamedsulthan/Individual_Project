377fb021b1a52cc40f444ed6c88344678fa5402d
Mohamed Sulthan <64527520+mohamedsulthan@users.noreply.github.com>
2023-09-25 18:09:08 +0530

    Delete Job_Board/backend directory

--- Job_Board/backend/services/routes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const userController = require("../models/controllers/UserControllers")
-
-router.post('/register', userController.registerUser)
-router.post('/login', userController.loginUser)
-router.get('/verify', userController.verify)
-
-module.exports = router;
